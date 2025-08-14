f5bee8861aa57044ee3e386f580587e47352de89
Rahul Vishwakarma <vishwarahul99@gmail.com>
2025-08-14 23:15:31 +0530

    26127

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -18,7 +18,7 @@ from django.shortcuts import render
 from django.contrib import admin
 from django.urls import path,include
 
-def  custom_404(request, exception):
+def  custom_page_not_found(request, exception):
     return  render(request,'404.html',status=404)
 urlpatterns = [
     path('admin/', admin.site.urls),
