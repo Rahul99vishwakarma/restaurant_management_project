012d3a40f3a2ababb93e817c0a07436e72c46230
Rahul Vishwakarma <vishwarahul99@gmail.com>
2025-08-15 23:00:47 +0530

    26129

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from django.shortcuts import render
 
-from django.shortcuts import render
+def homepage(request):
+    context = {
+        'restaurant_name': settings.RESTAURANT_NAME
 
-def about(request):
-    return render(request,'about.html')
+    }
+    return render(request, 'index.html',context)
