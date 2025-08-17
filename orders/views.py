d9703953177884957efb4719fb7446ef1f39a8ca
Rahul Vishwakarma <vishwarahul99@gmail.com>
2025-08-17 14:31:47 +0530

    33793

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-from django.conf import settings
 
-def home_view(request):
-    context = {
-        "restaurant_name": settings.RESTAURANT_NAME
-    }
-    return render(request, "index.html", context)
+def contact(request):
+    return render(request, "contact.html")
