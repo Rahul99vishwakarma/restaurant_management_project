a55f8b6b7a8bfe5eca217789465d14cfa183aa86
Rahul Vishwakarma <vishwarahul99@gmail.com>
2025-09-04 18:32:06 +0530

    38979

--- products/models.py
+++ products/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-# Create your models here.
-class Item(models.Model):
-    item_name = models.CharField(max_length=150)
-    item_price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
+class menuitems(models.models):
+    name=models.CharField(max_length=100)
+    description = models.textField()
+    price = models.DecimalField(max_DIGIT=6, decimal_places=2)
 
     def __str__(self):
-        return str(self.item_name)
+        return self.name
